cb08aa68e6aa989d66f3a46ea8aaefe34e1e1ec0
Yashrajsinh Jadeja <46606790+yashrajsinh@users.noreply.github.com>
2025-02-22 02:17:02 -0500

    Buttons Add
    Buttons Changes according to pages also animations added as well

--- Core/Home/ViewModels/HomeView.swift
+++ Core/Home/ViewModels/HomeView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfdolio: Bool = false
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            //Background Layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            //Content layer
+            VStack{
+                //calling homeheader 
+                homeheader
+                
+                Spacer(minLength: 0)
+                
+            }
+            
+        }
     }
 }
 
 #Preview {
-    HomeView()
+    NavigationView{
+        HomeView()
+            .navigationBarHidden(true)
+    }
+    
+}
+
+extension HomeView{
+    private var homeheader: some View{
+        HStack{
+            //Conditation for chnage views
+            CircleButtonView(iconName: showPortfdolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfdolio)
+                )
+            Spacer()
+            Text(showPortfdolio ? "Portfolio  💰" : "Stock Feed  📈")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            
+            Spacer()
+            //changing buttond depending on state
+            CircleButtonView(iconName: showPortfdolio ? "arrow.forward" : "wallet.pass")
+                .rotationEffect(Angle(degrees: showPortfdolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring){
+                        showPortfdolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
 }
